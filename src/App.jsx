929fd017e8f466b71445d7cbdaf8ee259d8296b7
sajim-khan <shossain1427@gmail.com>
2023-04-02 02:16:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+    <Navbar></Navbar>
+      <h1 className="text-7xl text-purple-700">Hello tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
