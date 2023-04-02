3b1b9f6b6e8a1f304748b1458c59a2ee05834fe3
sajim-khan <shossain1427@gmail.com>
2023-04-02 16:51:38 +0600

    added Create Nav toggle and navbar animation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
+import Link from "../Link/Link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-const routes = [
-  { id: 1, name: "Home", path: "/" },
-  { id: 2, name: "About", path: "/about" },
-  { id: 3, name: "Contact", path: "/contact" },
-  { id: 4, name: "Blog", path: "/blog" },
-  { id: 5, name: "Blog Post", path: "/blog/:postId" },
-  { id: 6, name: "Login", path: "/login" },
-  { id: 7, name: "Register", path: "/register" }
-];
+  const routes = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Contact", path: "/contact" },
+    { id: 4, name: "Blog", path: "/blog" },
+    { id: 5, name: "Blog Post", path: "/blog/:postId" },
+    { id: 6, name: "Login", path: "/login" },
+    { id: 7, name: "Register", path: "/register" },
+  ];
 
+  return (
+    <nav className="bg-purple-500 color:blue">
+      <div onClick={() => setOpen(!open)} className="md:hidden">
+        <span>
+          {open === true ? (
+            <XMarkIcon className="h-6 w-6 text-blue-500" />
+          ) : (
+            <Bars3Icon className="h-6 w-6 text-blue-500" />
+          )}
+        </span>
+      </div>
 
-    return (
-        <nav>
-            <ul>
-                {
-                    routes.map(route => <li>{route.name}</li>)
-                }
-            </ul>
-        </nav>
-    );
+      <ul className={`md:flex absolute md:static duration-500 bg-cyan-400 pl-8 pb-4 py-2 ${open ? 'top-6' : '-top-48'}`}>
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
